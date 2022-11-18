1797eab2de2eec1d63f7eb4a16ca40d552a6b561
clint360 <clint360.rebase@gmail.com>
2022-11-17 18:14:23 -0800

    started editing app's javascript file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,9 @@
 // DOM Elements
+const acFL = document.querySelector('.ac');
+const pmEL = document.querySelector('.pm')
+const percentEl = document.querySelector('.percent');
+
+const additionEL = document.querySelector('.addition');
+const subtractionEL = document.querySelector('.subtration');
+const multiplicationEL = document.querySelector('.multiplication');
+const multiplicationEL = document.querySelector('.multiplication');
